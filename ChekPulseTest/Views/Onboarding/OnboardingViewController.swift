60e30c444040fdaece1e7f588bd1f4281ba4f3cd
vladimir-lugin <vladimir.lugin@moneyman.ru>
2023-07-31 22:23:14 +0300

    onboarding init

---
+++ ChekPulseTest/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingViewController.swift
+//  ChekPulseTest
+//
+//  Created by lugin on 31.07.23.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var continueButton: UIButton!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func continueButtAction(_ sender: Any) {
+    }
+}
